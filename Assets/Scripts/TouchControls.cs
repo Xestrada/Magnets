dfca701a141b9bee8d42be9cef2249089e1a449f
Xestrada <Xestrada.Jorge@gmail.com>
2017-05-18 17:36:58 -0700

    Touch Controls

---
+++ Assets/Scripts/TouchControls.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TouchControls : MonoBehaviour {
+
+    public GameObject magnet;
+	
+	void Update () {
+
+        //This makes sure that there is a touch on the screen
+		if(Input.touchCount > 0)
+        {
+            ///This gets the first touch and turns the pixel coordinates into game orl coordinates
+            magnet.transform.position = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+        }
+
+        //Mouse controls
+        magnet.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+}
