6b59f7ef8de1fe8dad848221d9527e00ea30261c
Xestrada <Xestrada.Jorge@gmail.com>
2017-06-02 13:56:22 -0700

    Touch Testing

---
+++ Assets/Scripts/Magnet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Magnet : MonoBehaviour {
+
+    float touchId = 0;
+
+    public Vector2 Position()
+    {
+        return transform.position;
+    }
+	
+	public void SetPosition(Vector2 pos)
+    {
+        transform.position = pos;
+    }
+
+    public float TouchID
+    {
+        get
+        {
+            return touchId;
+        }
+        set
+        {
+            touchId = value;
+        }
+    }
+}
--- Assets/Scripts/TouchControls.cs
+++ Assets/Scripts/TouchControls.cs
@@ -2,18 +2,55 @@
 
 public class TouchControls : MonoBehaviour {
 
-    public GameObject magnet;
-	
+    public Magnet[] magnets;
+    bool allOff = true;
+
+    //int FindUnusedMagnet;
+
+
 	void Update () {
 
         //This makes sure that there is a touch on the screen
-		if(Input.touchCount > 0 && CompareTag("Particle"))
+        if (Input.touchCount > 0)
         {
+            allOff = false;
             ///This gets the first touch and turns the pixel coordinates into game orl coordinates
-            magnet.transform.position = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-        }
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                if (i <= 4)
+                {
+                    if (magnets[i].TouchID == 0 && !magnets[i].gameObject.activeSelf)
+                    {
+                        magnets[i].gameObject.SetActive(true);
+                        magnets[i].TouchID = Input.GetTouch(i).fingerId;
 
-            magnet.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    }
+                    if (magnets[i].TouchID == Input.GetTouch(i).fingerId)
+                    {
+                        magnets[0].SetPosition(Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position));
+                    }
+                    else if (magnets[i].TouchID != 0)
+                    {
+                        magnets[i].TouchID = 0;
+                        magnets[i].gameObject.SetActive(false);
+                    }
+
+                }
+            }
+        }
+        else if (!allOff)
+        {
+            for (int i = 0; i < magnets.Length; i++)
+            {
+                magnets[i].gameObject.SetActive(false);
+            }
+        }
+        //Remove this after finishing
+        else
+        {
+            magnets[0].gameObject.SetActive(true);
+            magnets[0].SetPosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
 
 	}
 }
